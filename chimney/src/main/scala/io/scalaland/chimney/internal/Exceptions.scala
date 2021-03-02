41de4613239dc85656439b18756c1f6a20a1151c
Yaroslav Krukovsky <iaroslavk@wix.com>
2021-03-02 12:31:37 +0200

    detail EnumUnrecognizedInstanceException message

--- chimney/src/main/scala/io/scalaland/chimney/internal/Exceptions.scala
+++ chimney/src/main/scala/io/scalaland/chimney/internal/Exceptions.scala
@@ -1,5 +1,5 @@
 package io.scalaland.chimney.internal
 
 case class EnumUnrecognizedInstanceException(sourceTypeName: String, targetTypeName: String) extends Exception(
-  s"mapping for 'Unrecognized' proto enum instance is abscent in $targetTypeName subtypes"
+  s"mapping for 'Unrecognized' $sourceTypeName proto enum instance is abscent in $targetTypeName subtypes"
 )
