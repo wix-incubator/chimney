f6856477fa073915cfc24bd206e04e10768e30a0
Ilya Doroshenko <illiad@wix.com>
2021-06-15 16:17:32 +0300

    added new SDL manual id generation types (#35)

--- chimney/src/test/scala/io/scalaland/chimney/examples/wix/SdlAnnotations.scala
+++ chimney/src/test/scala/io/scalaland/chimney/examples/wix/SdlAnnotations.scala
@@ -2,17 +2,15 @@ package com.wixpress.infra.sdl.api.id //important, checked by the macro guards
 
 import scala.annotation.StaticAnnotation
 
-case class id(length: IdLength = UUIDCompatible, idGeneration: IdGeneration = IdGeneration.Auto)
-    extends StaticAnnotation
-
-sealed trait IdLength
-case object UUIDCompatible extends IdLength
-case object WixDataCompatible extends IdLength
+case class id(idGeneration: IdGeneration = IdGeneration.Auto) extends StaticAnnotation
 
 sealed trait IdGeneration
 object IdGeneration {
   case object Auto extends IdGeneration
   case object Manual extends IdGeneration
+  case object ManualUUID extends IdGeneration
+  case class ManualWithCustomLength(length: Int) extends IdGeneration
+  case object ManualWixDataCompatible extends IdGeneration
 
   case object SomeNewIdGenerationType extends IdGeneration //not in SDL, used to test the possible addition of new types
 }
