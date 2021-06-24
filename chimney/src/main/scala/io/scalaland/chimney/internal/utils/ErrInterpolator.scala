45420be76b7645e13177f31cb4e3a5a41e0ab1ab
Dmytro Kostiuchenko <dmytroko@wix.com>
2021-06-24 21:18:26 +0300

    BUMP

--- chimney/src/main/scala/io/scalaland/chimney/internal/utils/ErrInterpolator.scala
+++ chimney/src/main/scala/io/scalaland/chimney/internal/utils/ErrInterpolator.scala
@@ -3,7 +3,7 @@ package io.scalaland.chimney.internal.utils
 import scala.StringContext.treatEscapes
 
 object ErrInterpolator {
-  val NewLine = "\u001E\n";
+  val NewLine = "\u200B\n"; // scalac doesn't allow non-printable chars apparently
 
   implicit class ErrInterpolator(private val sc: StringContext) extends AnyVal {
     def err(args: Any*): String = sc.standardInterpolator(treatEscapes, args).split('\n').mkString(NewLine)
