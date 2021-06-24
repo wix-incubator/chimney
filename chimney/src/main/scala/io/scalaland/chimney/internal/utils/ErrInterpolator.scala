90e7d159f8c1e0fbe105b340644fdeea3fcb8eeb
Dmytro Kostiuchenko <dmytroko@wix.com>
2021-06-24 20:32:29 +0300

    BUMP

--- chimney/src/main/scala/io/scalaland/chimney/internal/utils/ErrInterpolator.scala
+++ chimney/src/main/scala/io/scalaland/chimney/internal/utils/ErrInterpolator.scala
@@ -3,7 +3,7 @@ package io.scalaland.chimney.internal.utils
 import scala.StringContext.treatEscapes
 
 object ErrInterpolator {
-  val NewLine = "\u200B\n";
+  val NewLine = "~\n";
 
   implicit class ErrInterpolator(private val sc: StringContext) extends AnyVal {
     def err(args: Any*): String = sc.standardInterpolator(treatEscapes, args).split('\n').mkString(NewLine)
